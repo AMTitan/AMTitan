7e0e6fa88779272024766470a8b631e38de059a1
AMTitan <amtitan@icloud.com>
2021-02-04 18:21:06 -0500

    got api working

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
+# <img src="https://emojis.slackmojis.com/emojis/images/1531849430/4246/blob-sunglasses.gif?1531849430" width="30"/> My name is Arthur Melton and I have ![Profile View Counter](https://komarev.com/ghpvc/?username=AMTitan) (Last updated on Thursday, February 4, 6:20 PM EST).
+<p align="center">
+<img style="width:100%" src="https://github-readme-stats.vercel.app/api?username=AMTitan&show_icons=true&bg_color=30,e96443,904e95&title_color=fff&text_color=fff" /> 
+</p>
+<p align="center">
+<img style="width:100%" src="https://github-readme-stats.vercel.app/api/top-langs/?username=AMTitan&layout=compact&bg_color=30,e96443,904e95&title_color=fff&text_color=fff" /> 
+</p>
+<p align="center">
+<img style="width:100%" src="https://github-readme-stats.vercel.app/api/wakatime?username=AMTitan&layout=compact&bg_color=30,e96443,904e95&title_color=fff&text_color=fff" />
+</p>
+
+# Fun Facts
+
+04d
+
+ • The weather for me its a overcast clouds and the temperature is 9.52°C (49.14°F) but it actually feels like 6.93°C (44.47°F)!
+ • The sun rise for me is at 07:31 AM and the sun set is at 06:10 PM!
+ • The wind for me  is 0.45 meters/sec (1.01mph)!
+ • The persaintage of the sky is filled with clouds for me is 100%! testers
 
--- index.js
+++ index.js
@@ -9,59 +9,77 @@ const MUSTACHE_MAIN_DIR = './main.mustache';
 */
 const request = require('request');
 
-let apiKey = ${{ secrets.WeatherApi }};
-let city = '30004';
-let url = `http://api.openweathermap.org/data/2.5/weather?zip=${city}&units=metric&appid=${apiKey}`
+let url = 'http://api.openweathermap.org/data/2.5/weather?zip=30004&units=metric&appid=8333920da3e5cd15901dca62f2b8d0f6';
 
-let rawdata = fs.readFileSync('weather.json');
-let body1 = JSON.parse(rawdata);
+var body1;
 
-fs.readFile("weather.json", JSON, function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  body1 = data;
+Promise.all(promises).then(() => {
+  res.render('index', {
+    posts: myPosts
+  });
+  res.end();
 });
 
-let DATA = {
-  name: 'Arthur Melton',
-  body: body1,
-  FerTem: Math.round((body1.main.temp * 1.8 + 32) * 100) / 100,
-  FerFel: Math.round((body1.main.feels_like * 1.8 + 32) * 100) / 100,
-  Des: body1.weather[0].description,
-  icon: body1.weather[0].icon,
-  SunRise: new Date(body1.sys.sunrise * 1000).toLocaleString('en-GB', {
-    hour: '2-digit',
-    minute: '2-digit',
-    timeZone: 'America/New_York',
-  }),
-  SunSet: new Date(body1.sys.sunset * 1000).toLocaleString('en-GB', {
-    hour: '2-digit',
-    minute: '2-digit',
-    timeZone: 'America/New_York',
-  }),
-  WSmph: Math.round((body1.wind.speed * 2.236936) * 100) / 100,
-  date: new Date().toLocaleDateString('en-GB', {
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    timeZoneName: 'short',
-    timeZone: 'America/New_York',
-  }),
-};
-
-/**
-  * A - We open 'main.mustache'
-  * B - We ask Mustache to render our file with the data
-  * C - We create a README.md file with the generated output
-  */
-function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
-    if (err) throw err;
-    const output = Mustache.render(data.toString(), DATA);
-    fs.writeFileSync('README.md', output);
+var promises = [];
+const promise = new Promise((resolve) => {
+  request(url, function (err, response, body) {
+    if (err) {
+      console.log('error:', error);
+    } else {
+      console.log('body:', body);
+      fs.writeFileSync("weather.json", body);
+      body1 = body;
+    }
   });
-}
-generateReadMe();
+})
+
+Promise.all(promises).then(() => {
+  let rawdata = fs.readFileSync('Weather.json');
+  body1 = JSON.parse(rawdata);
+
+  console.log(body1);
+
+  let DATA = {
+    name: 'Arthur Melton',
+    body: body1,
+    FerTem: Math.round((body1.main.temp * 1.8 + 32) * 100) / 100,
+    FerFel: Math.round((body1.main.feels_like * 1.8 + 32) * 100) / 100,
+    Des: body1.weather[0].description,
+    icon: body1.weather[0].icon,
+    SunRise: new Date(body1.sys.sunrise * 1000).toLocaleString('en-GB', {
+      hour: '2-digit',
+      minute: '2-digit',
+      timeZone: 'America/New_York',
+    }),
+    SunSet: new Date(body1.sys.sunset * 1000).toLocaleString('en-GB', {
+      hour: '2-digit',
+      minute: '2-digit',
+      timeZone: 'America/New_York',
+    }),
+    WSmph: Math.round((body1.wind.speed * 2.236936) * 100) / 100,
+    date: new Date().toLocaleDateString('en-GB', {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      timeZoneName: 'short',
+      timeZone: 'America/New_York',
+    }),
+  };
+
+  /**
+   * A - We open 'main.mustache'
+   * B - We ask Mustache to render our file with the data
+   * C - We create a README.md file with the generated output
+   */
+  function generateReadMe() {
+    fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+      if (err) throw err;
+      const output = Mustache.render(data.toString(), DATA);
+      fs.writeFileSync('README.md', output);
+    });
+  }
+  generateReadMe();
+});
+
